bfe41a4b3f0488cc72fb2ee92045e05151f9fbe0
Alexandre Previatti <previatti@gmail.com>
2021-10-02 11:58:41 -0400

    TimeZoneList

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace TimeZoneList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlyCollection<TimeZoneInfo> tzCollection;
+            tzCollection = TimeZoneInfo.GetSystemTimeZones();
+
+            foreach (TimeZoneInfo timeZone in tzCollection)
+            {
+                if (timeZone.DisplayName.Contains("-03"))
+                    Console.WriteLine("   {0}: {1}", timeZone.Id, timeZone.DisplayName);
+            }
+                
+        }
+    }
+}
